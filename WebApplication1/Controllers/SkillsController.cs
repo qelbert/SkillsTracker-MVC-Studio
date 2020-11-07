5f6c49391c666d5b9d86a80ac1ef57a74c268620
Queue <elbertq2@gmail.com>
2020-11-06 20:16:25 -0600

    begun studio

---
+++ WebApplication1/Controllers/SkillsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // GET: /<controller>
+            string html1 = "<h1>Skills Tracker</h1>" +
+                "<h2>Gettin' it Done, One Skill at-a-Time</h2>" +
+                "<ol>" +
+                "<li>JavaScript</li>" +
+                "<li>HTML</li>" +
+                "<li>Angular</li>" +
+                "</ol>";
+
+
+            return Content (html1, "text/html");
+        }
+    }
+}
