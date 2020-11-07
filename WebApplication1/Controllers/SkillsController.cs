98b1006ecd4ac808d6d965471ae6e19989c45ae2
Queue <elbertq2@gmail.com>
2020-11-06 21:07:43 -0600

    added form to studio

--- WebApplication1/Controllers/SkillsController.cs
+++ WebApplication1/Controllers/SkillsController.cs
@@ -11,7 +11,8 @@ namespace WebApplication1.Controllers
         public IActionResult Index()
         {
             // GET: /<controller>
-            string html1 = "<h1>Skills Tracker</h1>" +
+            //[Route("Skillllllzzzz")]
+            string html = "<h1>Skills Tracker</h1>" +
                 "<h2>Gettin' it Done, One Skill at-a-Time</h2>" +
                 "<ol>" +
                 "<li>JavaScript</li>" +
@@ -20,7 +21,54 @@ namespace WebApplication1.Controllers
                 "</ol>";
 
 
-            return Content (html1, "text/html");
+            return Content (html, "text/html");
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            string html1 = 
+                "<form method = 'post' action = '/skills/form'>" +
+                "<label><b>Date</b></label><br>" +
+                "<input type='date' name = 'date' value = 'date'/>" +
+                "<fieldset>" +
+                "<legend> JavaScript </legend>" +
+                "<p>" +
+                "<label>Select Level </label>" +
+                "<select id = 'js_list'>" +
+                "<option value = '1'>Foundational</option>" +
+                "<option value = '2'>Intermediate</option>" +
+                "<option value = '3'>Advanced</option>" +
+                "</select>" +
+                "</p>" +
+                "</fieldset>" +
+                "<br>" +
+                "<fieldset>" +
+                "<legend> HTML </legend>" +
+                "<p>" +
+                "<label>Select Level </label>" +
+                "<select id = 'html_list'>" +
+                "<option value = '1'>Foundational</option>" +
+                "<option value = '2'>Intermediate</option>" +
+                "<option value = '3'>Advanced</option>" +
+                "</select>" +
+                "</p>" +
+                "</fieldset>" +
+                "<br>" +
+                "<fieldset>" +
+                "<legend> Angular </legend>" +
+                "<p>" +
+                "<label>Select Level </label>" +
+                "<select id = 'angular_list'>" +
+                "<option value = '1'>Foundational</option>" +
+                "<option value = '2'>Intermediate</option>" +
+                "<option value = '3'>Advanced</option>" +
+                "</select>" +
+                "</p>" +
+                "</fieldset>" +
+                "</form>";
+
+            return Content(html1, "text/html");
         }
     }
 }
